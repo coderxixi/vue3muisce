b07c0e64c49aa675f605869718377eebeaff0e09
liuss7 <976344695@qq.com>
2022-10-20 23:15:32 +0800

    播放歌曲前进与后退逻辑的开发

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -18,13 +18,13 @@
             <i class="icon-sequence"></i>
           </div>
           <div class="icon i-left">
-            <i class="icon-prev"></i>
+            <i class="icon-prev" @click="prev"></i>
           </div>
-          <div class="icon i-center" @click="togglePaly">
-            <i :class="playIcon"></i>
+          <div class="icon i-center">
+            <i :class="playIcon" @click="togglePaly"></i>
           </div>
           <div class="icon i-right">
-            <i class="icon-next"></i>
+            <i class="icon-next" @click="next"></i>
           </div>
           <div class="icon i-right">
             <i class="icon-not-favorite"></i>
@@ -53,12 +53,12 @@ const fullScreen = computed(() => {
 const currentSong = computed(() => {
   return store.currentSong;
 });
-const playing=computed(()=>{
-  return store.playing
-})
-const playIcon=computed(()=>{
-  return playing.value?'icon-pause':'icon-play'
-})
+const playing = computed(() => {
+  return store.playing;
+});
+const playIcon = computed(() => {
+  return playing.value ? "icon-pause" : "icon-play";
+});
 watch(currentSong, (newSong) => {
   if (newSong.id || !newSong.url) {
     return;
@@ -67,20 +67,66 @@ watch(currentSong, (newSong) => {
   audioEl.src = newSong.url;
   audioEl.play();
 });
-watch(playing,(newPlaying)=>{
+watch(playing, (newPlaying) => {
   const audioEl = audioRef.value;
-  newPlaying?audioEl.play():audioEl.pasue()
-})
-const goBlack=()=>{
-  store.setFullscreen(false)
-}
+  newPlaying ? audioEl.play() : audioEl.pasue();
+});
+const goBlack = () => {
+  store.setFullscreen(false);
+};
+//获取当前播放歌曲的索引
+const currentIndex = computed(() => {
+  return store.currentIndex;
+});
+const palyList = computed(() => {
+  return store.playList;
+});
 // 切换播放状态
-const togglePaly=()=>{
-  store.setPlayingState(!playing.value)
-}
-const pasue=()=>{
-  store.setPlayingState(false)
-}
+const togglePaly = () => {
+  store.setPlayingState(!playing.value);
+};
+const pasue = () => {
+  store.setPlayingState(false);
+};
+const prev = () => {
+  const list = palyList.value;
+  let index = currentIndex.value - 1;
+  if(!list.length) return
+  // 判断是否只有一首歌曲
+  if (list.length == 1) {
+    //循环播放
+    loop();
+  } else {
+    if (index === -1) {
+      index = list.length - 1;
+    }
+    store.setCurrentIndex(index);
+    if (!playing.value) {
+      store.setPlayingState(true);
+    }
+  }
+};
+const next = () => {
+  const list = palyList.value;
+  let index = currentIndex.value + 1;
+  if(!list.length) return
+  if (list.length === 1) {
+    loop();
+  } else {
+    if (index === list.length) {
+      index = 0;
+    }
+    store.setCurrentIndex(index);
+    if (!playing.value) {
+      store.setPlayingState(true);
+    }
+  }
+};
+const loop = () => {
+  const audioEl = audioRef.value;
+  audioEl.currentTime = 0;
+  audioEl.play();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -139,23 +185,24 @@ const pasue=()=>{
         color: $color-text;
       }
     }
-    .bottom{
-      position:absolute;
+    .bottom {
+      position: absolute;
       bottom: 50px;
       width: 100%;
-      .operators{
+      .operators {
         display: flex;
         align-items: center;
-        .icon{
+        .icon {
           flex: 1;
           color: $color-theme;
-          &.disable{
-            color:$color-theme-d
+          text-align: center;
+          &.disable {
+            color: $color-theme-d;
           }
-          i{
+          i {
             font-size: 30px;
           }
-          .i-left{
+          .i-left {
             text-align: right;
           }
         }
