980a2b2d87f90a88d180ff5cafc5c1af9aaed71c
liuss7 <976344695@qq.com>
2022-10-20 22:39:29 +0800

    播放器基础样式及播放歌曲功能开发

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Hearder from '@/components/header/header.vue'
     import Tabs from "@/components/tab/tab.vue"
+    import Player from "@/components/player/player.vue"
 </script>
 
 <template>
@@ -9,6 +10,8 @@
    <Tabs/>
    <!-- 视图 -->
    <router-view></router-view>
+   <!-- 播放器 -->
+   <Player/>
   </div>
 </template>
 
---
+++ src/components/player/player.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="player">
+    <div class="normal-player" v-show="fullScreen">
+      <div class="background">
+        <img :src="currentSong?.pic" alt="" />
+      </div>
+      <div class="top">
+        <div class="back" @click="goBlack">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title">{{ currentSong?.name }}</h1>
+        <h2 class="subtitle">{{ currentSong?.singer }}</h2>
+      </div>
+    </div>
+    <audio ref="audioRef"></audio>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, computed, watch } from "vue";
+import { useStore } from "@/store/index.js";
+const audioRef = ref(null);
+const store = useStore();
+const props = defineProps({
+  currentSong: {
+    type: Object,
+    default: {},
+  },
+});
+const fullScreen = computed(() => {
+  return store.fullScreen;
+});
+const currentSong = computed(() => {
+  return store.currentSong;
+});
+watch(currentSong, (newSong) => {
+  if (newSong.id || !newSong.url) {
+    return;
+  }
+  const audioEl = audioRef.value;
+  audioEl.src = newSong.url;
+  audioEl.play();
+});
+const goBlack=()=>{
+  store.setFullscreen(false)
+}
+</script>
+
+<style lang="scss" scoped>
+.player {
+  .normal-player {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 150px;
+    background: $color-background;
+    .background {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      opacity: 0.6;
+      filter: blur(20px);
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .top {
+      position: relative;
+      margin-bottom: 25px;
+      .back {
+        position: absolute;
+        top: 0;
+        left: 6px;
+        z-index: 50;
+      }
+      .icon-back {
+        display: block;
+        padding: 9px;
+        font-size: $font-size-large-x;
+        color: $color-theme;
+        transform: rotate(-90deg);
+      }
+      .title {
+        width: 70%;
+        margin: 0 auto;
+        line-height: 40px;
+        text-align: center;
+        @include no-wrap();
+        font-size: $font-size-large;
+        color: $color-text;
+      }
+      .subtitle {
+        line-height: 20px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+    }
+  }
+}
+</style>
