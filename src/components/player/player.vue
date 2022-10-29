c88abec4ac661ca60bc149d6625cc7e2a60fdfc9
liuss7 <976344695@qq.com>
2022-10-29 21:20:48 +0800

    进度条逻辑开发

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -14,11 +14,17 @@
       <!-- 按键 -->
       <div class="bottom">
         <div class="progress-wrapper">
-          <span class="time time-l">{{formatTime(currentTime)}}</span>
+          <span class="time time-l">{{ formatTime(currentTime) }}</span>
           <div class="progress-bar-wrapper">
-            <ProgressBar :progress="progress"/>
+            <ProgressBar
+              :progress="progress"
+              @progressChanging="onProgressChangeing"
+              @progressChanged="onProgressChanged"
+            />
           </div>
-          <span class="time time-r">{{ formatTime(currentSong?.duration) }}</span>
+          <span class="time time-r">{{
+            formatTime(currentSong?.duration)
+          }}</span>
         </div>
         <div class="operators">
           <div class="icon i-left">
@@ -34,28 +40,37 @@
             <i class="icon-next" @click="next"></i>
           </div>
           <div class="icon i-right">
-            <i :class="getFavoriteIcon(currentSong)" @click="toggleFavorite(currentSong)"></i>
+            <i
+              :class="getFavoriteIcon(currentSong)"
+              @click="toggleFavorite(currentSong)"
+            ></i>
           </div>
         </div>
       </div>
     </div>
-   
-    <audio ref="audioRef" @pause="pasue" @timeupdate="updateTime"></audio>
+
+    <audio
+      ref="audioRef"
+      @pause="pasue"
+      @timeupdate="updateTime"
+      @ended="end"
+    ></audio>
   </div>
 </template>
 
 <script setup>
 import { ref, defineProps, computed, watch } from "vue";
 import { useStore } from "@/store/index.js";
-import useMode from "./use-mode.js"
-import {formatTime} from "@/assets/js/util.js"
-import useFavorite from "./use-favorite.js"
-import ProgressBar from "./progress-bar.vue"
-const {modeIcon,changeMode}=useMode()
-const {getFavoriteIcon,toggleFavorite}=useFavorite()
+import useMode from "./use-mode.js";
+import { PLAY_MODE } from "@/assets/js/constant.js";
+import { formatTime } from "@/assets/js/util.js";
+import useFavorite from "./use-favorite.js";
+import ProgressBar from "./progress-bar.vue";
+const { modeIcon, changeMode } = useMode();
+const { getFavoriteIcon, toggleFavorite } = useFavorite();
 const audioRef = ref(null);
-
-const currentTime=ref(0)
+const progressChanging = ref(false);
+const currentTime = ref(0);
 const store = useStore();
 const props = defineProps({
   currentSong: {
@@ -63,9 +78,9 @@ const props = defineProps({
     default: {},
   },
 });
-const progress=computed(()=>{
+const progress = computed(() => {
   // return currentTime.value/ currentSong.value.duration
-})
+});
 const fullScreen = computed(() => {
   return store.fullScreen;
 });
@@ -78,11 +93,12 @@ const playing = computed(() => {
 const playIcon = computed(() => {
   return playing.value ? "icon-pause" : "icon-play";
 });
+const playMode = computed(() => store.playMode);
 watch(currentSong, (newSong) => {
   if (newSong.id || !newSong.url) {
     return;
   }
-  currentTime.value=0
+  currentTime.value = 0;
   const audioEl = audioRef.value;
   audioEl.src = newSong.url;
   audioEl.play();
@@ -111,7 +127,7 @@ const pasue = () => {
 const prev = () => {
   const list = palyList.value;
   let index = currentIndex.value - 1;
-  if(!list.length) return
+  if (!list.length) return;
   // 判断是否只有一首歌曲
   if (list.length == 1) {
     //循环播放
@@ -129,7 +145,7 @@ const prev = () => {
 const next = () => {
   const list = palyList.value;
   let index = currentIndex.value + 1;
-  if(!list.length) return
+  if (!list.length) return;
   if (list.length === 1) {
     loop();
   } else {
@@ -147,240 +163,265 @@ const loop = () => {
   audioEl.currentTime = 0;
   audioEl.play();
 };
-const updateTime=(e)=>{
-  currentTime.value=e.target.currentTime
-}
+const updateTime = (e) => {
+  if (!progressChanging.value) {
+    currentTime.value = e.target.currentTime;
+  }
+};
+const onProgressChangeing = (progress) => {
+  progressChanging.value = true;
+  currentTime.value = currentSong.value.duration * progress;
+};
+const onProgressChanged = (progress) => {
+  progressChanging.value = false;
+  audioRef.value.currentTime = currentTime.value =
+    currentSong.value.duration * progress;
+  if (!playing.value) {
+    store.setPlayingState(true);
+  }
+};
+const end = () => {
+  currentTime.value = 0;
+  if (playMode.value === PLAY_MODE.loop) {
+    loop();
+  } else {
+    next();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
- .player {
-    .normal-player {
-      position: fixed;
+.player {
+  .normal-player {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 150;
+    background: $color-background;
+    .background {
+      position: absolute;
       left: 0;
-      right: 0;
       top: 0;
-      bottom: 0;
-      z-index: 150;
-      background: $color-background;
-      .background {
-        position: absolute;
-        left: 0;
-        top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      opacity: 0.6;
+      filter: blur(20px);
+
+      img {
         width: 100%;
         height: 100%;
-        z-index: -1;
-        opacity: 0.6;
-        filter: blur(20px);
-
-        img {
-          width: 100%;
-          height: 100%;
-        }
       }
-      .top {
+    }
+    .top {
+      position: relative;
+      margin-bottom: 25px;
+      .back {
+        position: absolute;
+        top: 0;
+        left: 6px;
+        z-index: 50;
+      }
+      .icon-back {
+        display: block;
+        padding: 9px;
+        font-size: $font-size-large-x;
+        color: $color-theme;
+        transform: rotate(-90deg);
+      }
+      .title {
+        width: 70%;
+        margin: 0 auto;
+        line-height: 40px;
+        text-align: center;
+        @include no-wrap();
+        font-size: $font-size-large;
+        color: $color-text;
+      }
+      .subtitle {
+        line-height: 20px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-text;
+      }
+    }
+    .middle {
+      position: fixed;
+      width: 100%;
+      top: 80px;
+      bottom: 170px;
+      white-space: nowrap;
+      font-size: 0;
+      .middle-l {
+        display: inline-block;
+        vertical-align: top;
         position: relative;
-        margin-bottom: 25px;
-        .back {
-          position: absolute;
-          top: 0;
-          left: 6px;
-          z-index: 50;
-        }
-        .icon-back {
-          display: block;
-          padding: 9px;
-          font-size: $font-size-large-x;
-          color: $color-theme;
-          transform: rotate(-90deg);
-        }
-        .title {
-          width: 70%;
-          margin: 0 auto;
-          line-height: 40px;
-          text-align: center;
-          @include no-wrap();
-          font-size: $font-size-large;
-          color: $color-text;
-        }
-        .subtitle {
-          line-height: 20px;
-          text-align: center;
-          font-size: $font-size-medium;
-          color: $color-text;
-        }
-      }
-      .middle {
-        position: fixed;
         width: 100%;
-        top: 80px;
-        bottom: 170px;
-        white-space: nowrap;
-        font-size: 0;
-        .middle-l {
-          display: inline-block;
-          vertical-align: top;
-          position: relative;
-          width: 100%;
-          height: 0;
-          padding-top: 80%;
-          .cd-wrapper {
-            position: absolute;
-            left: 10%;
-            top: 0;
-            width: 80%;
-            box-sizing: border-box;
+        height: 0;
+        padding-top: 80%;
+        .cd-wrapper {
+          position: absolute;
+          left: 10%;
+          top: 0;
+          width: 80%;
+          box-sizing: border-box;
+          height: 100%;
+          .cd {
+            width: 100%;
             height: 100%;
-            .cd {
+            border-radius: 50%;
+            img {
+              position: absolute;
+              left: 0;
+              top: 0;
               width: 100%;
               height: 100%;
+              box-sizing: border-box;
               border-radius: 50%;
-              img {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                box-sizing: border-box;
-                border-radius: 50%;
-                border: 10px solid rgba(255, 255, 255, 0.1);
-              }
-              .playing {
-                animation: rotate 20s linear infinite
-              }
+              border: 10px solid rgba(255, 255, 255, 0.1);
             }
-          }
-          .playing-lyric-wrapper {
-            width: 80%;
-            margin: 30px auto 0 auto;
-            overflow: hidden;
-            text-align: center;
-            .playing-lyric {
-              height: 20px;
-              line-height: 20px;
-              font-size: $font-size-medium;
-              color: $color-text-l;
+            .playing {
+              animation: rotate 20s linear infinite;
             }
           }
         }
-        .middle-r {
-          display: inline-block;
-          vertical-align: top;
-          width: 100%;
-          height: 100%;
-          overflow: hidden;
-          .lyric-wrapper {
-            width: 80%;
-            margin: 0 auto;
-            overflow: hidden;
-            text-align: center;
-            .text {
-              line-height: 32px;
-              color: $color-text-l;
-              font-size: $font-size-medium;
-              &.current {
-                color: $color-text;
-              }
-            }
-            .pure-music {
-              padding-top: 50%;
-              line-height: 32px;
-              color: $color-text-l;
-              font-size: $font-size-medium;
-            }
-          }
-        }
-      }
-      .bottom {
-        position: absolute;
-        bottom: 50px;
-        width: 100%;
-        .dot-wrapper {
-          text-align: center;
-          font-size: 0;
-          .dot {
-            display: inline-block;
-            vertical-align: middle;
-            margin: 0 4px;
-            width: 8px;
-            height: 8px;
-            border-radius: 50%;
-            background: $color-text-l;
-            &.active {
-              width: 20px;
-              border-radius: 5px;
-              background: $color-text-ll;
-            }
-          }
-        }
-        .progress-wrapper {
-          display: flex;
-          align-items: center;
+        .playing-lyric-wrapper {
           width: 80%;
-          margin: 0px auto;
-          padding: 10px 0;
-          .time {
-            color: $color-text;
-            font-size: $font-size-small;
-            flex: 0 0 40px;
-            line-height: 30px;
-            width: 40px;
-            &.time-l {
-              text-align: left;
-            }
-            &.time-r {
-              text-align: right;
-            }
-          }
-          .progress-bar-wrapper {
-            flex: 1;
-          }
-        }
-        .operators {
-          display: flex;
-          align-items: center;
-          .icon {
-            flex: 1;
-            color: $color-theme;
-            &.disable {
-              color: $color-theme-d;
-            }
-            i {
-              font-size: 30px;
-            }
-          }
-          .i-left {
-            text-align: right;
-          }
-          .i-center {
-            padding: 0 20px;
-            text-align: center;
-            i {
-              font-size: 40px;
-            }
-          }
-          .i-right {
-            text-align: left
-          }
-          .icon-favorite {
-            color: $color-sub-theme;
+          margin: 30px auto 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .playing-lyric {
+            height: 20px;
+            line-height: 20px;
+            font-size: $font-size-medium;
+            color: $color-text-l;
           }
         }
       }
-      &.normal-enter-active, &.normal-leave-active {
-        transition: all .6s;
-        .top, .bottom {
-          transition: all .6s cubic-bezier(0.45, 0, 0.55, 1);
-        }
-      }
-      &.normal-enter-from, &.normal-leave-to {
-        opacity: 0;
-        .top {
-          transform: translate3d(0, -100px, 0);
-        }
-        .bottom {
-          transform: translate3d(0, 100px, 0)
+      .middle-r {
+        display: inline-block;
+        vertical-align: top;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        .lyric-wrapper {
+          width: 80%;
+          margin: 0 auto;
+          overflow: hidden;
+          text-align: center;
+          .text {
+            line-height: 32px;
+            color: $color-text-l;
+            font-size: $font-size-medium;
+            &.current {
+              color: $color-text;
+            }
+          }
+          .pure-music {
+            padding-top: 50%;
+            line-height: 32px;
+            color: $color-text-l;
+            font-size: $font-size-medium;
+          }
         }
       }
     }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .dot-wrapper {
+        text-align: center;
+        font-size: 0;
+        .dot {
+          display: inline-block;
+          vertical-align: middle;
+          margin: 0 4px;
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          background: $color-text-l;
+          &.active {
+            width: 20px;
+            border-radius: 5px;
+            background: $color-text-ll;
+          }
+        }
+      }
+      .progress-wrapper {
+        display: flex;
+        align-items: center;
+        width: 80%;
+        margin: 0px auto;
+        padding: 10px 0;
+        .time {
+          color: $color-text;
+          font-size: $font-size-small;
+          flex: 0 0 40px;
+          line-height: 30px;
+          width: 40px;
+          &.time-l {
+            text-align: left;
+          }
+          &.time-r {
+            text-align: right;
+          }
+        }
+        .progress-bar-wrapper {
+          flex: 1;
+        }
+      }
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
+      }
+    }
+    &.normal-enter-active,
+    &.normal-leave-active {
+      transition: all 0.6s;
+      .top,
+      .bottom {
+        transition: all 0.6s cubic-bezier(0.45, 0, 0.55, 1);
+      }
+    }
+    &.normal-enter-from,
+    &.normal-leave-to {
+      opacity: 0;
+      .top {
+        transform: translate3d(0, -100px, 0);
+      }
+      .bottom {
+        transform: translate3d(0, 100px, 0);
+      }
+    }
   }
+}
 </style>
--- src/components/player/progress-bar.vue
+++ src/components/player/progress-bar.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="progress-bar" >
+  <div class="progress-bar" @click="onClick">
     <div class="bar-inner">
       <div class="progress" :style="progressStyle" ref="barRef"></div>
-      <div class="progress-btn-wrapper" :style="btnStyle" 
+      <div
+        class="progress-btn-wrapper"
+        :style="btnStyle"
         @touchstart.prevent="onTouchstart"
         @touchmove="onTouchMove"
         @touchend="onTouchEnd"
-        >
+      >
         <div class="progress-btn"></div>
       </div>
     </div>
@@ -14,85 +16,100 @@
 </template>
 
 <script setup>
-import {defineProps,ref,watch,getCurrentInstance,onMounted,computed,useAttrs} from "vue";
-const barRef=ref(null)
-const progressBtnWidth=16
-const instant=getCurrentInstance()
-const offset=ref(0);
-let touch=ref({x1:'',beginWidth:0})
-const props=defineProps({
-  progress:{
-      type:Number,
-      default:0
-  }
-})
-onMounted(()=>{
+import {
+  defineProps,
+  ref,
+  watch,
+  getCurrentInstance,
+  onMounted,
+  computed,
+  defineEmits,
+} from "vue";
+const barRef = ref(null);
+const progressBtnWidth = 16;
+const instant = getCurrentInstance();
+const offset = ref(0);
+let touch = ref({ x1: "", beginWidth: 0 });
+const emit = defineEmits(["progress-changing", "progress-changed"]);
+const props = defineProps({
+  progress: {
+    type: Number,
+    default: 0,
+  },
+});
+onMounted(() => {
   // console.log('barRef',barRef.value.clientWidth);
-})
-watch(props.progress,(newprogress)=>{
-  console.log('newprogress',newprogress);
-  console.log('$el',instant.refs.ctx.$el);
-  const barWidth= instant.refs.ctx?.$el.clientWidth- progressBtnWidth    //15||barRef.value.clientWidth-progressBtnWidth;
-  offset.value=barWidth*newprogress
-})
-const progressStyle=computed(()=>{
-   return `width:${offset.value}px`
-})
-const btnStyle=computed(()=>{
-  return `transform:translate3d(${offset.value}px,0,0)`
-}
- 
-)
+});
+watch(props.progress, (newprogress) => {
+  console.log("newprogress", newprogress);
+  console.log("$el", instant.refs.ctx.$el);
+  const barWidth = instant.refs.ctx?.$el.clientWidth - progressBtnWidth; //15||barRef.value.clientWidth-progressBtnWidth;
+  offset.value = barWidth * newprogress;
+});
+const progressStyle = computed(() => {
+  return `width:${offset.value}px`;
+});
+const btnStyle = computed(() => {
+  return `transform:translate3d(${offset.value}px,0,0)`;
+});
 
-const onTouchstart=(e)=>{
-  touch.value.x1=e.touches[0].pageX
-  touch.value.beginWidth=barRef.value.clientWidth
-}
-const onTouchMove=(e)=>{
-   const delta=e.touches[0].pageX-touch.value.x1;
-   const tempWidth=touch.value.beginWidth+delta;
-   const barWidth= instant.ctx?.$el.clientWidth- progressBtnWidth;
-   const progress=Math.min(1, Math.max(tempWidth/barWidth,0));
-   offset.value=barWidth*progress
-
-  
-}
-const onTouchEnd=()=>{
-  
-}
+const onTouchstart = (e) => {
+  touch.value.x1 = e.touches[0].pageX;
+  touch.value.beginWidth = barRef.value.clientWidth;
+};
+const onTouchMove = (e) => {
+  const delta = e.touches[0].pageX - touch.value.x1;
+  const tempWidth = touch.value.beginWidth + delta;
+  const barWidth = instant.ctx?.$el.clientWidth - progressBtnWidth;
+  const progress = Math.min(1, Math.max(tempWidth / barWidth, 0));
+  offset.value = barWidth * progress;
+  emit("progress-changing", progress);
+};
+const onTouchEnd = () => {
+  const barWidth = instant.ctx?.$el.clientWidth - progressBtnWidth;
+  const progress = barRef.value.clientWidth / barWidth;
+  emit("progress-changed", progress);
+};
+const onClick = (e) => {
+  const rect = instant.ctx?.$el.getBoundingClientRect();
+  const offsetWidth = e.pageX - rect.left;
+  const barWidth = instant.ctx?.$el.clientWidth - progressBtnWidth;
+  const progress = offsetWidth / barWidth;
+  emit("progress-changed", progress);
+};
 </script>
 
 <style lang="scss" scoped>
-  .progress-bar{
-    height: 30px;
-    .bar-inner{
-      position: relative;
-      top: 13px;
-      height: 4px;
-      background: rgba($color: #000000, $alpha: .3);
-      .progress{
-        position: absolute;
-        height: 100%;
+.progress-bar {
+  height: 30px;
+  .bar-inner {
+    position: relative;
+    top: 13px;
+    height: 4px;
+    background: rgba($color: #000000, $alpha: 0.3);
+    .progress {
+      position: absolute;
+      height: 100%;
+      background: $color-theme;
+    }
+    .progress-btn-wrapper {
+      // position: absolute;
+      left: -8px;
+      top: -13px;
+      width: 30px;
+      height: 30px;
+      .progress-btn {
+        position: relative;
+        top: -7px;
+        left: -7px;
+        box-sizing: border-box;
+        width: 16px;
+        height: 16px;
+        border: 3px solid $color-text;
+        border-radius: 50%;
         background: $color-theme;
-      }
-      .progress-btn-wrapper{
-        // position: absolute;
-        left: -8px;
-        top: -13px;
-        width: 30px;
-        height: 30px;
-        .progress-btn{
-          position: relative;
-          top: -7px;
-          left: -7px;
-          box-sizing: border-box;
-          width: 16px;
-          height: 16px;
-          border: 3px solid $color-text;
-          border-radius: 50%;
-          background: $color-theme;
-        }
       }
     }
   }
+}
 </style>
